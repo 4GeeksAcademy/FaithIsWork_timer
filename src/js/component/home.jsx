6e6c39822166fbf9efad78e16d4ad6fedca6cddc
FaithIsWork11 <faithiswork@duck.com>
2024-07-11 16:29:27 +0000

    finsh

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,17 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+import React, { useState, useEffect } from 'react';
+function SecondsCounter() {
+  const [secondsLabel, setSecondsLabel] = useState('000000');
+  useEffect(() => {
+    const startTime = Date.now();
+    const interval = setInterval(() => {
+      const currentTime = Date.now();
+      const elapsedSeconds = Math.floor((currentTime - startTime) / 1000);
+      setSecondsLabel(String(elapsedSeconds).padStart(6, '0'));
+    }, 1000);
+    return () => clearInterval(interval); // Cleanup interval on unmount
+  }, []);
+  return (
+    <div className="timer"> {secondsLabel}</div>
+  );
+}
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,6 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "../styles/index.css"
+import SecondsCounter from './component/home';
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
-
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+ReactDOM.render(<SecondsCounter />, document.getElementById('app'));
